e4d0a37f8e29b0d1c30fb88464eb670876b3187b
Kian Acaster <acasterkian@gmail.com>
2019-11-30 15:05:33 +0000

    Update sketch.js

--- Tic Tac Toe/sketch.js
+++ Tic Tac Toe/sketch.js
@@ -1,147 +1,140 @@
-/* Tic Tac Toe in JavaScript using p5.js (by Kian Acaster)*\
-*  Current state:
-*  The code's horrific and there's a ton of bugs, the canvas system doesn't work properly and it doesn't switch turns. 
-*  I'll likely end up rewriting most of it.
-*  -- Aims for this project:
-*  • Working CP vs User,
-*  • Online gameplay (maybe),
-*  • Eventually converting to native JS (just for the sake of it),
-*  -- [Personal Goals] -- 
-*  • Learn more JavaScript,
-*  • Be more efficient with code,	
-*  • Actually finish a project.
-*/
+// Old project, bodged, works though.
 
+let turn = 0;
+let grid = [0,0,0,0,0,0,0,0,0];
 
-var turn = 0;
-var board = ["E","E","E","E","E","E","E","E","E"];	 
-var board;
-var position = [];
-var positions = [0,1,2,3,4,5,6,7,8];
-var indPos;
-var gridPosX; 
-var gridPosY;
 function setup(){
 	createCanvas(610,610);
-	for(var i = 0; i < width; i += width / 3){
+	for(let i = 0; i < width; i += width / 3){
 		for(var j = 0; j < height; j += height / 3){
 			rect(i,j,width / 3,height / 3);
 		}
 	}
 }
-function draw(){
-	for(var i = 0; i < board.length; i++){
-		if(board[i] == "X"){
-			stroke(255,0,0);
-			strokeWeight(5);
-			cross(gridPosX, gridPosY, gridPosX + width / 3, gridPosY + width / 3);	
-		}else if(board[i] == "O"){	
-			stroke(0);
-			ellipse(gridPosX + (width / 3) / 2, gridPosY + (width / 3) / 2, (width / 3) - 10, (height / 3) - 10);
-		}
-	}
-}	
+
 function cross(x1,y1,x2,y2){
 	line(x1,y1,x2,y2);
 	line(x2,y1,x1,y2);
 }
 
 function mousePressed(){
+	let gridPosX; 
+	let gridPosY;
+	let Xsq;
+	let Ysq;
+	
 	if(mouseX > 0 && mouseX < width / 3){
 		gridPosX = 0;
-		position.push(0);
+		Xsq = 0;
 	}else if(mouseX > width / 3 && mouseX < (width / 3) * 2){
 		gridPosX = width / 3;
-		position.push(1);
+		Xsq = 1;
 	}else if(mouseX > (width / 3) * 2 && mouseX < width){
 		gridPosX = (width / 3) * 2
-		position.push(2);
+		Xsq = 2;
 	}
+	
 	if(mouseY > 0 && mouseY < height / 3){
 		gridPosY = 0;
-		position.push(0);
+		Ysq = 0;
 	}else if(mouseY > height/ 3 && mouseY < (height / 3) * 2){
 		gridPosY = height / 3;
-		position.push(1);
+		Ysq = 1;
 	}else if(mouseY > (height / 3) * 2 && mouseY < height){
-		gridPosY = (height / 3) * 2
-		position.push(2);		
+		gridPosY = (height / 3) * 2;
+		Ysq = 2;	
 	}
-	if(position[0] == 0 && position[1] == 0){
-		indPos = 0;
-		if(turn == 0){
-			board[0] = "X";
-		}else if(turn == 1){
-			board[0] == "O";
-		}
+	
+	if(turn == 0 && grid[checkSquare(Xsq, Ysq)] == 0){
+		grid[checkSquare(Xsq, Ysq)] = 1;
+		stroke(255,0,0);
+		strokeWeight(5);
+		cross(gridPosX, gridPosY, gridPosX + width / 3, gridPosY + width / 3);
+		turn++;
+	}else if(turn == 1 && grid[checkSquare(Xsq, Ysq)] == 0){
+		grid[checkSquare(Xsq, Ysq)] = 2;
+		stroke(0);
+		ellipse(gridPosX + (width / 3) / 2, gridPosY + (width / 3) / 2, (width / 3) - 10, (height / 3) - 10);
+		turn--;
 	}
-	if(position[0] == 0 && position[1] == 1){
-		indPos = 3;
-		if(turn == 0){
-			board[3] = "X";
-		}else if(turn == 1){ 
-			board[3] == "O";
-		}
-	}
-	if(position[0] == 0 && position[1] == 2){
-		indPos = 6;
-		if(turn == 0){
-			board[6] = "X";
-		}else if(turn == 1){
-			board[6] == "O";
-		}	
-	}
-	if(position[0] == 1 && position[1] == 0){
-		indPos = 2;
-		if(turn == 0){
-			board[2] = "X";
-		}else if(turn == 1){
-			board[2] == "O";
-		}
-	}
-	if(position[0] == 1 && position[1] == 1){
-		indPos = 4;
-		if(turn == 0){
-			board[4] = "X";
-		}else if(turn == 1){
-			board[4] == "O";
-		}	
-	}
-	if(position[0] == 1 && position[1] == 2){
-		indPos = 7;
-		if(turn == 0){
-			board[7] = "X";
-		}else if(turn == 1){
-			board[7] == "O";
-		}	
-	}
-	if(position[0] == 2 && position[1] == 0){
-		indPos = 2;
-		if(turn == 0){
-			board[2] = "X";
-		}else if(turn == 1){
-			board[2] == "O";
-		}	
-	}
-	if(position[0] == 2 && position[1] == 1){
-		indPos = 5;
-		if(turn == 0){
-			board[5] = "X";
-		}else if(turn == 1){
-			board[5] == "O";
-		}	
-	}
-	if(position[0] == 2 && position[1] == 2){
-		indPos = 8;
-		if(turn == 0){
-			board[8] = "X";
-		}else if(turn == 1){
-			board[8] == "O";
-		}	
-	}
-	if(turn == 0){ turn = 1; }else if(turn == 1){ turn = 0; }
+	
+	if(checkWin() == 1) console.log("X wins");
+	if(checkWin() == 2) console.log("O wins");
 }
 
-function mouseReleased(){
-	position.length = 0;
+function checkSquare(x, y){
+	let square;
+	if(x == 0 && y == 0) square = 0;
+	if(x == 1 && y == 0) square = 1;
+	if(x == 2 && y == 0) square = 2;
+	if(x == 0 && y == 1) square = 3;
+	if(x == 1 && y == 1) square = 4;
+	if(x == 2 && y == 1) square = 5;
+	if(x == 0 && y == 2) square = 6;
+	if(x == 1 && y == 2) square = 7;
+	if(x == 2 && y == 2) square = 8;
+
+	return square;
+}
+
+function checkWin(){
+	if(grid[0] == 1 && grid[1] == 1 && grid[2] == 1){
+		return 1;
+	}
+	else if(grid[3] == 1 && grid[4] == 1 && grid[5] == 1){
+		return 1;
+	}
+	else if(grid[6] == 1 && grid[7] == 1 && grid[8] == 1){
+		return 1;
+	}
+	else if(grid[0] == 1 && grid[4] == 1 && grid[8] == 1){
+		return 1;
+	}
+	else if(grid[6] == 1 && grid[4] == 1 && grid[2] == 1){
+		return 1;
+	}
+
+	if(grid[0] == 1 && grid[3] == 1 && grid[6] == 1){
+		return 1;
+	}
+	else if(grid[1] == 1 && grid[4] == 1 && grid[7] == 1){
+		return 1;
+	}
+	else if(grid[2] == 1 && grid[5] == 1 && grid[8] == 1){
+		return 1;
+	}
+
+
+
+
+
+
+	if(grid[0] == 2 && grid[1] == 2 && grid[2] == 2){
+		return 2;
+	}
+	else if(grid[3] == 2 && grid[4] == 2 && grid[5] == 2){
+		return 2;
+	}
+	else if(grid[6] == 2 && grid[7] == 2 && grid[8] == 2){
+		return 2;
+	}
+	else if(grid[0] == 2 && grid[4] == 2 && grid[8] == 2){
+		return 2;
+	}
+	else if(grid[6] == 2 && grid[4] == 2 && grid[2] == 2){
+		return 2;
+	} if(grid[0] == 2 && grid[3] == 2 && grid[6] == 2){
+		return 2;
+	}
+	else if(grid[1] == 2 && grid[4] == 2 && grid[7] == 2){
+		return 2;
+	}
+	else if(grid[2] == 2 && grid[5] == 2 && grid[8] == 2){
+		return 2;
+	}
+
+	else{
+
+		return false;
+	}
 }
